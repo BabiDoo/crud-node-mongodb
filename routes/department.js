8b216be17f1e155c1a488b980ae39d21023937fa
Barbara <barbara.laisdorneles@gmail.com>
2025-05-20 08:05:03 -0300

    quickfix

--- routes/department.js
+++ routes/department.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Department = require('/models/department');
+const Department = require('../models/Department');
 
 router.post('/', async (req, res) => {
   try {
