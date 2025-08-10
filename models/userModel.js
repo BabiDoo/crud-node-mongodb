e8cb6c8face10aac8272758a106895c20835d8fc
Barbara <barbara.laisdorneles@gmail.com>
2025-08-10 18:29:55 -0300

    feat(models): add Department model export

---
+++ models/userModel.js
@@ -0,0 +1,34 @@
+
+//import mongoose and uses Schema property
+const mongoose = require('mongoose');
+const  { Schema } = mongoose;
+
+const UserSchema = new Schema (
+    {
+        name: {
+            type: String,
+            require: true,
+        },
+        surname: {
+            type: String,
+            require: true,
+        },
+        department: {
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: `departmentModel`,
+            required: true
+        }
+
+        
+
+    },
+    {timestamps : true} //auto update the timestamp when inserting/updating
+);
+
+
+//create and exports model
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
+
+
+
